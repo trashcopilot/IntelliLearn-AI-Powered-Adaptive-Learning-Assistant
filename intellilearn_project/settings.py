82337efb2b4bd8c483286a41ce7036cd3055a298
trashcopilot <trashcopilot@users.noreply.github.com>
2026-05-05 23:46:43 +0800

    feat: switch Django database backend to MySQL

--- intellilearn_project/settings.py
+++ intellilearn_project/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'intellilearn_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
